133626266c57ddcc11316def41ff0893088bd31a
goncalo-meneses <goncalomeneses@tecnico.ulisboa.pt>
2025-04-05 14:30:27 +0100

    Fixed real values as entries for state vector

--- dejmps/app_bob.py
+++ dejmps/app_bob.py
@@ -14,8 +14,8 @@ from netqasm.sdk.external import NetQASMConnection, Socket, get_qubit_state
 #     return (ketbra(ket_0, np.transpose(ket_0)) + ketbra(ket_1, np.transpose(ket_1))) / 2
 
 def bell_state():
-    ket_0 = [[1], [0]]
-    ket_1 = [[0], [1]]
+    ket_0 = [[1.+0.j], [0.j]]
+    ket_1 = [[0.j], [1.+0.j]]
     
     ket_00 = np.kron(ket_0, ket_0)
     ket_11 = np.kron(ket_1, ket_1)
@@ -23,8 +23,11 @@ def bell_state():
     return (ket_00 + ket_11) / np.sqrt(2)
 
 def fidelity(state, dm):
-    bra = np.transpose(state)
-    return np.dot(bra, np.dot(dm, state))
+    state = state.reshape(-1, 1)
+    bra = state.conj().T
+    result = np.dot(bra, np.dot(dm, state))
+    return np.real_if_close(result.item())
+
 
 def main(app_config=None):
     socket = Socket("bob", "alice", log_config=app_config.log_config)
@@ -33,18 +36,26 @@ def main(app_config=None):
 
     bob = NetQASMConnection("bob", log_config=app_config.log_config, epr_sockets=[epr_socket])
 
+    phi_00 = bell_state()
+
     with bob:
         epr_1, epr_2 = epr_socket.recv(number=2)
 
-        succ = dejmps_protocol_bob(epr_1, epr_2, bob, socket)
-        dens_matrix = get_qubit_state(epr_1, reduced_dm=False)
+        # dens_in = get_qubit_state(epr_1, reduced_dm=False)
 
-        phi_00 = bell_state()
-        f = fidelity(phi_00, dens_matrix)
+        # f_in = fidelity(phi_00, dens_in)
+
+        succ = dejmps_protocol_bob(epr_1, epr_2, bob, socket)
+
+        dens_out = get_qubit_state(epr_1, reduced_dm=False)
+
+        
+        f_out = fidelity(phi_00, dens_out)
     
     if succ:    
         print("Bob succeeded :-)")
-        print("The fidelity of the final state is:", f)
+        # print("The fidelity of the final state is:", f_in)
+        print("The fidelity of the final state is:", f_out)
     else:
         print("Bob did not succceed ;-(")
 
